37b0c044ec5ca3b215787a25831ad45c9cf8b780
ItzKiwiSky <kiwisky94@gmail.com>
2026-01-08 19:35:57 -0300

    new challenge

--- game/main.lua
+++ game/main.lua
@@ -87,9 +87,9 @@ function love.initialize()
                     ["ladies_night"] = false,
                     ["double_trouble"] = false,
                     ["reaction_test"] = false,
+                    ["strings_of_the_past"] = false,
                 }
             }
-
         }
     }
 
